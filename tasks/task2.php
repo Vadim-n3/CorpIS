6f8a8d069f81a794c02807d6a69cd49a497fa78c
Vadim-n3 <ironman50@yandex.ru>
2020-03-10 20:34:27 +0300

    task2
    Inserting in an URL missing spec. symbols

---
+++ tasks/task2.php
@@ -0,0 +1,40 @@
+﻿<?php
+	// программа вставляет в url пропущенные '://', '.' перед доменом и '/' после домена, если требуется
+
+	$handle = fopen("input.txt", "r");
+	$buffer = fgets($handle);
+
+	$pos = strpos($buffer, "https");
+
+	if ($pos !== false) {
+		$buffer = substr($buffer, 0, 5) . "://" . substr($buffer, 5);
+	} else {
+		$buffer = substr($buffer, 0, 4) . "://" . substr($buffer, 4);
+	}
+
+	$posRu = strpos($buffer, "ru");
+	$posCom = strpos($buffer, "com");
+	$domLen = 0;
+
+	if ($posRu !== false) {
+		$pos = $posRu;
+		$domLen = 2;
+	} else {
+		$pos = $posCom;
+		$domLen = 3;
+	}
+
+	if (strlen($buffer) > ($pos + $domLen)){
+		$buffer = substr($buffer, 0, $pos + $domLen) . "/" . substr($buffer, $pos + $domLen);
+	}
+
+	$buffer = substr($buffer, 0, $pos) . "." . substr($buffer, $pos);
+
+	echo $buffer;
+
+
+
+	$fd = fopen("output.txt", 'w') or die("не удалось создать файл");
+	fwrite($fd, $buffer);
+	fclose($fd);
+?>
